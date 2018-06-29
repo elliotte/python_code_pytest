24fb099a8b18562b7163fc1c8a344219fdea8112
imark.get@gmail.com <mark@f6s.com>
2018-06-29 15:04:40 +0100

    decorators confusing

---
+++ tests/test_decorators.py
@@ -0,0 +1,12 @@
+# dont' understand...
+
+# from lib.decorators import *
+
+# def test_decorator():
+# 	@my_decorator
+# 	def new_function
+# 		return "What?"
+
+#     result = new_function()
+
+#     assert result == ""
